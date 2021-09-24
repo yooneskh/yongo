02ea8b57693faf694e946c68162dcbe714fe5972
Yoones Khoshghadam <yooneskh@gmail.com>
2021-09-24 18:51:49 +0330

    feat (yuery): basic populate

--- examples/02-simple-usage.ts
+++ examples/02-simple-usage.ts
@@ -1,9 +1,12 @@
+// deno-lint-ignore-file no-explicit-any
+
 import { Yonnection, Yuery } from '../mod.ts';
 import { log } from '../util/log.ts';
 
 
 await new Yonnection('mongo://127.0.0.1:27017/test').connect();
 await new Yuery('users').deleteMany();
+await new Yuery('homes').deleteMany();
 
 
 const insertQuery = new Yuery('users');
@@ -15,12 +18,25 @@ const madeUser = await insertQuery.insert();
 log({ madeUser });
 
 const query = new Yuery('users');
-query.with({ name: 'Yoones' });
-const usersWithNameYoones = await query.query();
-log({ usersWithNameYoones });
+query.where({ name: 'Yoones' });
+const userswhereNameYoones = await query.query();
+log({ userswhereNameYoones });
 
 const updateQuery = new Yuery('users');
-updateQuery.with({ name: 'Yoones' });
+updateQuery.where({ name: 'Yoones' });
 updateQuery.put('middleName', 'the313th');
 const editResult = await updateQuery.commit();
 log({ editResult });
+
+
+const newHomeQuery = new Yuery('homes');
+newHomeQuery.put('name', 'New Home');
+newHomeQuery.put('owner', String((madeUser as any)._id));
+const madeHome = await newHomeQuery.insert();
+log({ madeHome });
+
+
+const getHomeQuery = new Yuery('homes');
+getHomeQuery.populate({ path: 'owner', collection: 'users' });
+const homes = await getHomeQuery.query();
+log({ homes });
--- lib/Yuery.ts
+++ lib/Yuery.ts
@@ -4,23 +4,30 @@ import { deepMerge, Database, Collection, ObjectId } from '../deps.ts';
 import { Yonnection, getDefaultYonnection } from './Yonnection.ts';
 
 
-export class Yuery {
+export interface IYueryPopulate {
+  path: string;
+  collection: string;
+  fields?: string;
+}
+
+
+export class Yuery<T> {
 
   private yonnection: Yonnection;
   private filters: any = {};
   private sorts: any = {};
   private payload: any = {};
-  private populates: any = {};
+  private populates: IYueryPopulate[] = [];
   private limit: number | undefined;
   private skip: number | undefined;
 
   private database: Database;
-  private collection: Collection<unknown>;
+  private collection: Collection<T>;
 
   constructor(collectionName: string, yonnection?: Yonnection) {
     this.yonnection = yonnection ?? getDefaultYonnection();
     this.database = this.yonnection.getClient().database();
-    this.collection = this.database.collection(collectionName);
+    this.collection = this.database.collection<T>(collectionName);
   }
 
 
@@ -50,8 +57,25 @@ export class Yuery {
     return payload;
   }
 
+  private async populateDcoument(doucment: any, keyPrefix = '') {
+    for (const key of Object.keys(doucment)) {
 
-  public with(query: any) {
+      const populate = this.populates.find(it => it.path === (!keyPrefix ? key : `${keyPrefix}.${key}`));
+      if (!populate) continue;
+
+      const query = new Yuery(populate.collection, this.yonnection);
+      query.where({ _id: new ObjectId(doucment[key]) }); // todo: select only wanted fields
+      doucment[key] = await query.queryOne();
+
+      if (doucment[key]) {
+        await this.populateDcoument(doucment[key], `${keyPrefix}.${key}`);
+      }
+
+    }
+  }
+
+
+  public where(query: any) {
     this.filters = deepMerge(this.filters, query);
   }
 
@@ -63,12 +87,12 @@ export class Yuery {
     this.payload[key] = value;
   }
 
-  public populate(path: string, fields?: string) {
-    this.populates[path] = fields ?? '';
+  public populate(...populates: IYueryPopulate[]) {
+    this.populates.push(...populates);
   }
 
 
-  public query(): Promise<any[]> {
+  public async query(): Promise<T[]> {
 
     const query = this.collection.find(this.filters);
 
@@ -76,21 +100,32 @@ export class Yuery {
     if (this.limit) query.limit(this.limit);
     if (this.skip) query.skip(this.skip);
 
-    return query.toArray();
+    const documents = await query.toArray();
+
+    await Promise.all(
+      documents.map(it => this.populateDcoument(it))
+    );
+
+    return documents;
 
   }
 
-  public queryOne(): Promise<any> {
-    return this.collection.findOne(this.filters);
+  public async queryOne(): Promise<T | undefined> {
+
+    const document = await this.collection.findOne(this.filters);
+
+    await this.populateDcoument(document);
+    return document;
+
   }
 
   public count(): Promise<number> {
     return this.collection.countDocuments(this.filters);
   }
 
-  public async insert(): Promise<any> {
+  public async insert(): Promise<T | undefined> {
     const id = await this.collection.insertOne(this.getNormalizedPayload());
-    return this.collection.findOne({ _id: id }); // todo: check if payload itself has all ids
+    return this.collection.findOne({ _id: id }); // todo: check if payload itself has id
   }
 
   public commitMany() {
